aa24e5adf2d117bb1eed9add63a0a44555493cc5
kafleganesh <kafleganesh1235@gmail.com>
2018-12-17 15:13:14 +0100

    Received name, average rating and description of series

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,42 +1,60 @@
 import React, { Component } from 'react';
-import Programs from '../components/program'
+// import Programs from '../components/program'
 
 class homePage extends Component {
-    state = {
-        name : undefined,
-    }
+   constructor(props){
+       super(props);
+       this.state = {
+           items : []
+       }
+   }
 
-    displayPrograms = async (e) => {
-        e.preventDefault();
+    // displayPrograms = async (e) => {
+    //     e.preventDefault();
 
-        const reqURL=`http://api.tvmaze.com/shows `;
-        const api_call = await fetch(reqURL);
-
-        const data = await api_call.json();
-
-        console.log(data);
-
-        this.setState({
-            name : data.name,
+    //     const reqURL=`http://api.tvmaze.com/shows `;
+    //     const api_call = await fetch(reqURL);
+    //     const data = await api_call.json();
+    //     console.log(data);
+    //     this.setState({
+    //         name : data.name,
             
-        });
-        console.log(this.name);
-        
-    }
-    
-    
+    //     });
+    //     console.log(this.name);
 
+    componentDidMount(){
+        fetch('http://api.tvmaze.com/shows')
+        .then(res => res.json())
+        .then(json =>{
+            this.setState({
+
+                items : json
+
+            })
+        });
+    }
+    // }
+    
     render() {
+        var { items} = this.state;
         return (
             <div>
                 <h1>
                     Tv Maze progrms
                 </h1>
                 <button onClick = {this.displayPrograms}>Look All the Programs</button>
-                <Programs
+                {/* <Programs
                     name = {this.state.name}
 
-                />
+                /> */}
+                <ul>
+                    {items.map(item =>(
+                        <p key = {item.id}>
+                        {item.name} | {item.rating.average} | {item.summary} | {item.image.medium}|{item.image.original}
+                        <button>Read more</button>
+                        </p>
+                    ))};
+                </ul>
             </div>
         );
     }
